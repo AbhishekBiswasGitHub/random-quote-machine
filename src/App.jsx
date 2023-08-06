4a5c4512c557b86736adf00fb0a6d5f5bb6b6618
abbup <abhishek.biswas.git@gmail.com>
2023-08-07 02:43:17 +0530

    Add component App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import "./App.css";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const getQuote = () => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then(({ data }) => {
+        setText(data.content);
+        setAuthor(data.author);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  useEffect(() => {
+    if (!text || !author) getQuote();
+  });
+
+  return <div id="quote-box"></div>;
+};
 
 export default App;
